a7ad5cf576089ffe12fd264b03d4c86768bc9623
Liu-201 <2119899903@qq.com>
2026-04-19 17:25:45 +0800

    解决后端问题

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
-// 使用 Railway 分配的端口，如果没有则默认 5000
-const PORT = process.env.PORT || 5000;
+// 使用 Railway 分配的端口，若无默认8080
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`✅ 服务器运行在端口 ${PORT}`);
